b372fa7fd679bc98e080aa0de7dfcd6c94bcf86d
MrCottrell <73645309+MrCottrell@users.noreply.github.com>
2020-10-31 14:07:22 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,24 @@
-function Set_Pixel (index: number, colour: number, Delay: number) {
+function Set_Pixel (index: number, colour: number, Delay: number, Message: string, Amber2: boolean) {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
     strip.setPixelColor(index, colour)
+    if (Amber2) {
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    }
     strip.show()
+    basic.showString(Message)
     basic.pause(Delay)
 }
 function Amber () {
-    Set_Pixel(1, neopixel.colors(NeoPixelColors.Yellow), 2000)
+    Set_Pixel(1, neopixel.colors(NeoPixelColors.Yellow), 2000, "STOP IF SAFE", false)
 }
 function Green () {
-    Set_Pixel(0, neopixel.colors(NeoPixelColors.Green), 2000)
+    Set_Pixel(0, neopixel.colors(NeoPixelColors.Green), 2000, "GO", false)
 }
 function Red () {
-    Set_Pixel(2, neopixel.colors(NeoPixelColors.Red), 2000)
+    Set_Pixel(2, neopixel.colors(NeoPixelColors.Red), 2000, "STOP", false)
 }
 function RedAmber () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-    strip.show()
+    Set_Pixel(2, neopixel.colors(NeoPixelColors.Red), 2000, "GET READY", true)
 }
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
@@ -25,15 +26,7 @@ strip.clear()
 strip.setBrightness(128)
 basic.forever(function () {
     Red()
-    basic.showString("STOP")
-    basic.pause(2000)
     RedAmber()
-    basic.showString("GET READY")
-    basic.pause(2000)
     Green()
-    basic.showString("GO")
-    basic.pause(2000)
     Amber()
-    basic.showString("STOP IF SAFE")
-    basic.pause(2000)
 })
